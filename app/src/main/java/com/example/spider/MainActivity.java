d79386b1d3f9e1d2ea7c1cf8f7f3c80f29df6210
Mrinmoy Kalita <mrin443@gmail.com>
2022-04-30 14:37:43 +0530

    Init

---
+++ app/src/main/java/com/example/spider/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.spider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread th = new Thread();
+        Log.i("info", "Running");
+    }
+}
